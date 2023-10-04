434497f35d8a51255e8a57f99e5b36c9ae0328d8
Longcat2957 <schrodingers030@gmail.com>
2023-10-04 15:17:06 +0900

    first commit

---
+++ network.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class modelA(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
+    
+class modelB(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self, x):
+        pass
